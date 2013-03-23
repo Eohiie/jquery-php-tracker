a1e35c9bd1bcddc2da06b14977138120d7dbb04b
Shane Clark <eohiie@gmail.com>
2013-03-22 21:27:42 -0600

    Updated the repo
    Updated the repo with completed files.

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+//This is the config file
+//Most of this is set by default
+
+//Database information
+$db_host="";
+$db_name="";
+$db_user="";
+$db_pass="";
+
+//Tracker user information
+$username="";
+$password="";
+
+//
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+//include("includes/config.php");
+//$db_host = $GLOBALS["db_host"];
+//$db_user = $GLOBALS["db_user"];
+//$db_pass = $GLOBALS["db_pass"];
+//$db_name = $GLOBALS["db_name"];
+?>
+<html>
+<head>
+<title><?php if(isset($GLOBALS["tr_title"])) echo $GLOBALS["tr_title"]." Statistics"; else echo "CCTracker Statistics";?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="style/base.css" />
+<link rel="stylesheet" href="style/fonts.css" />
+</head>
+<body>
+<div id="yif-1"><!-- Header -->
+<h1></h1>
+	</div>
+  
+<div id="yif-2"><!-- Side Bar -->
+
+	</div>
+  
+<div id="yif-3"><!-- Content -->
+
+	</div>
+  
+<div id="yif-4"><!-- Footer -->
+<center>Based on PHPBTTracker. This tracker is released under GPL code</center>
+	</div>
+</body>
+</html>
